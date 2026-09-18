6d7f205efb08c35d89d61ce826100b131f9f2a23
crsto22 <arturo200512@gmail.com>
2026-09-17 21:19:52 -0500

    Limpiar cliente seleccionado al finalizar una venta

--- app/ventas/page.tsx
+++ app/ventas/page.tsx
@@ -1198,6 +1198,7 @@ export default function VentasPage() {
 
   const handleSaleSuccess = () => {
     setCartItems([]);
+    setSelectedClient(null);
     setDiscountValue("");
     setSaleNote("");
     setIsDiscountEditorOpen(false);
